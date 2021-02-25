63c06d8f84e1da71a6dbaa05cb991291bb6a1370
Khrystyna <khrystyna.skobelska@gmail.com>
2021-02-25 22:17:07 +0200

    Addimg unity project test

---
+++ Assets/TestingEventSubscriber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingEventSubscriber : MonoBehaviour
+{
+    [SerializeField] private TestingEventSystem testES;
+
+    // Update is called once per frame
+    void Start()
+    {
+        testES.SetCubeColor(Color.green , () => { Debug.Log("The colr was changed successfully!"); });
+    }
+
+    
+}
---
+++ Assets/TestingEventSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingEventSystem : MonoBehaviour
+{
+    private Action OnSetColor;
+    public Renderer cubeRenderer;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnSetColor();
+        }
+    }
+
+    public void SetCubeColor(Color color, Action OnSetColor)
+    {
+        this.OnSetColor = OnSetColor;
+        cubeRenderer.material.color = color;
+    }
+    
+        
+    
+}
